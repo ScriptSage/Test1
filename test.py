840c1ea1145b9cfde38a24caf853e261f3811e64
Changhun Kim <kory89@gmail.com>
2025-02-02 20:42:07 +0900

    sort 구현현

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
+import random
+
 print('Hello world!')
 print('Welcome to new world.')
 print(1+2)
 
+numbers = [random.randint(1, 100) for _ in range(10)]
+sorted_numbers = sorted(numbers)
+
+print("Unsorted numbers:", numbers)
+print("Sorted numbers:", sorted_numbers)
